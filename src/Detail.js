492b35993e3dc5bd59ee12fff486963fe4589e7b
Lown Kim <wideplate3@gmail.com>
2021-10-19 17:55:08 +0900

    feat: 일정시간경과후 알림창 삭제 기능 구현

--- src/Detail.js
+++ src/Detail.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+
+let 박스 = styled.div`
+  padding-top: 30px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25;
+  color: ${(props) => props.색상};
+`;
 
 function Detail(props) {
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState("");
+
+  useEffect(() => {
+    let 타이머 = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    return () => {
+      clearTimeout(타이머); //setTimeOut주의점 : 컴포넌트 사라질때 타이머 해제하기. 코드꼬임 버그예방
+    };
+  }, []);
+
   let { id } = useParams();
+  let history = useHistory();
   let 찾은상품 = props.data.find(function (상품) {
     return 상품.id == id;
   });
 
-  let history = useHistory();
   return (
     <div className="container">
+      <박스>
+        <제목>Detail</제목>
+      </박스>
+      {inputData}
+      <input
+        onChange={(e) => {
+          inputData변경(e.target.value);
+        }}
+      />
+
+      {alert === true ? (
+        <div className="my-alert2">
+          <p>재고가 얼마 남지 않았습니다.</p>
+        </div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
           <img
